78989af4722bb07f87f0b6ac2630cad92c54ec28
Gnurt1087 <thanhtrung101087@gmail.com>
2026-05-18 14:07:55 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { MotionConfig } from 'framer-motion'
 
